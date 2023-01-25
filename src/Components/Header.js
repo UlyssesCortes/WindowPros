07307c3d84967a4c0dc828fbef7a5f2dcd83ae07
root <root@DESKTOP-30JE0LL>
2023-01-25 15:21:56 -0600

    Specials Header styled

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -3,24 +3,25 @@ import ReactDOM from 'react-dom';
 import { Link } from 'react-router-dom';
 import './header.css'
 
+
 const Header = () => {
     return (
         <>
             <nav className='navHome'>
                 <section className='navHeader'>
-                    <h1 className=' titleHome'>WindowPros</h1>
+                    <h1 className=' titleHome whiteTittle'>WindowPros</h1>
                     <ul className='navListHome'>
-                        <li><Link className='linksHome homeLInk' to='/'>Home</Link ></li>
-                        <li><Link className='linksHome' to='/Services'>Services</Link ></li>
+                        <li><Link className='linksHome servicesLink' to='/'>Home</Link ></li>
+                        <li><Link className='linksHome servicesLink servicesSelected' to='/Services'>Services</Link ></li>
                         {/* <li><Link className='links' to='/OurClients'>Our Clients</Link ></li> */}
-                        <li><Link className='linksHome' to='/Specials'>Specials</Link ></li>
-                        <li><Link className='linksHome' to='/ContactUs'>ContactUs</Link ></li>
+                        <li><Link className='linksHome servicesLink specSelecter' to='/Specials'>Specials</Link ></li>
+                        <li><Link className='linksHome servicesLink' to='/ContactUs contactSelected'>ContactUs</Link ></li>
                     </ul>
                 </section>
                 <section className='navButton'>
                     <ul className='navListHome'>
-                        <li><Link className='linkBtn'>Sign In</Link></li>
-                        <li><Link className='linkBtn singUpBtn'>Sign Up Free</Link></li>
+                        <li><Link className='linkBtn secStyleBtn'>Sign In</Link></li>
+                        <li><Link className='linkBtn singUpBtn secStyleBtn secStyleBtnSignUp'>Sign Up Free</Link></li>
                     </ul>
                 </section>
             </nav>
