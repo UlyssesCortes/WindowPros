ba7358a31766b86b09a73948e86ecf498efc03c8
root <root@DESKTOP-30JE0LL>
2023-01-27 02:13:10 -0600

    Avatar added to Contact us

--- src/Components/ContactUs.js
+++ src/Components/ContactUs.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Header from './Header';
-
+import Spline from '@splinetool/react-spline';
+import './contact.css'
 
 const ContactUs = () => {
     return (
@@ -10,6 +11,21 @@ const ContactUs = () => {
                 <div className='waveOutline'></div>
                 <Header />
                 <h1 className='slogan'>Get in Touch with Us</h1>
+
+                <div className='contactFormSec'>
+                    <div className='avatar'>
+                        <Spline scene="https://prod.spline.design/nO3fW5qBd6sKhKZL/scene.splinecode" />
+                    </div>
+                    <form className='formSec'>
+                        <section>
+                            <input placeholder='Name'></input>
+                            <input placeholder='Email'></input>
+                        </section>
+                        <input placeholder='Subject'></input>
+                        <input placeholder='Message'></input>
+                    </form>
+                </div>
+
             </body>
 
         </>
