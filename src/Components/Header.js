51ae925c22a6f18453d8a479cad20be3cf974e07
root <root@DESKTOP-30JE0LL>
2023-01-27 21:17:05 -0600

    Scrolling issue fixed

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -13,7 +13,6 @@ const Header = () => {
                     <ul className='navListHome'>
                         <li><Link className=' links' to='/'>Home</Link ></li>
                         <li><Link className=' links' to='/Services'>Services</Link ></li>
-                        {/* <li><Link className='links' to='/OurClients'>Our Clients</Link ></li> */}
                         <li><Link className=' links ' to='/Specials'>Specials</Link ></li>
                         <li><Link className=' links ' to='/ContactUs'>ContactUs</Link ></li>
                     </ul>
@@ -25,6 +24,7 @@ const Header = () => {
                     </ul>
                 </section>
             </nav>
+            <a className='phoneLink' href="tel:+1234567890"><p className='myNum'>760-685-4653</p></a>
         </>
     )
 }
