0ddcd5a247df59311bddbf68b1a39c47d9dd7ef8
root <root@DESKTOP-30JE0LL>
2023-01-28 02:44:24 -0600

    Contact form updated

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -14,7 +14,7 @@ const Home = () => {
                         <div className='orangeTool'></div>
                         <h1 className=' titleHome'>WindowPros</h1>
                         <ul className='navListHome'>
-                            <li><Link className='linksHome homeLInk' to='/'>Home</Link ></li>
+                            <li><Link id="homeLink" to='/'>Home</Link ></li>
                             <li><Link className='linksHome' to='/Services'>Services</Link ></li>
                             <li><Link className='linksHome' to='/Specials'>Specials</Link ></li>
                             <li><Link className='linksHome' to='/ContactUs'>ContactUs</Link ></li>
@@ -22,7 +22,7 @@ const Home = () => {
                     </section>
                     <section className='navButton'>
                         <ul className='navListHome'>
-                            <li><Link className='linkBtn'>Sign In</Link></li>
+                            <li><Link className='linkBtn signInBtn'>Sign In</Link></li>
                             <li><Link className='linkBtn singUpBtn'>Sign Up Free</Link></li>
                         </ul>
                     </section>
