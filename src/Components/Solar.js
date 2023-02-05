75cb21c5f3b67bb51abc992c0eded454deb6b25e
root <root@DESKTOP-30JE0LL>
2023-02-05 14:52:35 -0600

    Solar page style added

--- src/Components/Solar.js
+++ src/Components/Solar.js
@@ -1,17 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
+
 import Header from "./Header";
+import './solar.css'
 
 const Solar = () => {
+    const [openForm, setOpenForm] = useState(false)
+    const btnClicked = () => {
+        setOpenForm(!openForm)
+    }
     return (
         <>
             <head>
                 <title>WindowPros | Specials-Solar</title>
             </head>
-            <body className='wave'>
-                <div className='waveOutline'></div>
+            <body>
+                <div class="hero-image">
+                    <Header />
+                    <h1 className='slogan'>Bringing clarity to your energy efficiency!</h1>
+                    <button onClick={() => btnClicked()} className='quoteBtnHome'>Get a quote now!</button>
+                </div>
+                {openForm &&
+                    <div class="containerModal" id="modal-opened">
+                        <div class="modal">
 
-                <Header />
-                <h1 className='slogan'>Bringing clarity to your energy efficiency!</h1>
+                            <div class="details">
+                                <h1 class="title">Get your quote</h1>
+                                <p class="description">Get an Accurate Quote, Quick and Easy!</p>
+                            </div>
+
+                            <p class="txt">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis ex dicta maiores libero minus obcaecati iste optio, eius labore repellendus.</p>
+
+                            <button class="btnModal">Button &rarr;</button>
+
+                            <button onClick={() => btnClicked()} class="link-2"></button>
+                        </div>
+                    </div>
+                }
                 <ul class="background">
                     <li></li>
                     <li></li>
